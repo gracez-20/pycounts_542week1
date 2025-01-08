2b62abf5cb510fab587d6ac039ad510f067c4c15
Zhiwei Zhang <graceeee.zhang@gmail.com>
2025-01-07 23:11:14 -0800

    docs: created docstrings for package functions

--- src/pycounts_542week1/plotting.py
+++ src/pycounts_542week1/plotting.py
@@ -1,7 +1,28 @@
 import matplotlib.pyplot as plt
 
 def plot_words(word_counts, n=10):
-    """Plot a bar chart of word counts."""
+    
+    """Plot a bar chart of word counts.
+    
+    Parameters
+    ----------
+    word_counts : collections.Counter
+        Counter object of word counts.
+    n : int, optional
+        Plot the top n words. By default, 10.
+
+    Returns
+    -------
+    matplotlib.container.BarContainer
+        Bar chart of word counts.
+
+    Examples
+    --------
+    >>> from pycounts.pycounts import count_words
+    >>> from pycounts.plotting import plot_words
+    >>> counts = count_words("text.txt")
+    >>> plot_words(counts)
+    """
     top_n_words = word_counts.most_common(n)
     word, count = zip(*top_n_words)
     fig = plt.bar(range(n), count)
@@ -9,3 +30,47 @@ def plot_words(word_counts, n=10):
     plt.xlabel("Word")
     plt.ylabel("Count")
     return fig
+
+def load_text(input_file):
+    """Load text from a text file and return as a string.
+
+    Parameters
+    ----------
+    input_file : str
+        Path to text file.
+
+    Returns
+    -------
+    str
+        Text file contents.
+
+    Examples
+    --------
+    >>> load_text("text.txt")
+    """
+    with open(input_file, "r") as file:
+        text = file.read()
+    return text
+
+def clean_text(text):
+    """Lowercase and remove punctuation from a string.
+
+    Parameters
+    ----------
+    text : str
+        Text to clean.
+
+    Returns
+    -------
+    str
+        Cleaned text.
+
+    Examples
+    --------
+    >>> clean_text("Early optimization is the root of all evil!")
+    'early optimization is the root of all evil'
+    """
+    text = text.lower()
+    for p in punctuation:
+        text = text.replace(p, "")
+    return text
